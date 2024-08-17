d18f83217e3b3afec8bd1197c728bd3024237631
KABANOFESTO <kabanofesto1@gmail>
2024-08-17 11:10:06 +0200

    ch-err

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const ussdRoute = require("./routers/ussd");
+const userRoute = require("./routers/userRouter");
+const messageRouter = require('./routers/messageRouter');
+const { dbConnect } = require("./config/db");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+dbConnect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", ussdRoute);
+app.use("/", messageRouter);
+app.use("/user", userRoute);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
